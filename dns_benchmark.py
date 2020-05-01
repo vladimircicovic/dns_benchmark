48a4da9fae96d04e10a7fbe4faf1895a45be83a4
Ashwin Vishnu <9155111+ashwinvis@users.noreply.github.com>
2020-05-01 18:20:06 +0200

    Lookup github.com, add plotting

--- dns_benchmark.py
+++ dns_benchmark.py
@@ -2,6 +2,7 @@
 import dns.resolver
 import dns.query
 import sys
+import matplotlib.pyplot as plt
 
 if len(sys.argv) < 2:
     sys.exit('Usage: python %s dns_list.txt' % sys.argv[0])
@@ -13,6 +14,10 @@ with open(sys.argv[1]) as f:
 min_response = 9999999
 min_response_server = ""
 
+times = []
+servers = []
+
+
 for dns_server in dns_list:
     dns_server = str(dns_server).strip('\n')
     resolver = dns.resolver.Resolver()
@@ -20,13 +25,22 @@ for dns_server in dns_list:
     resolver.lifetime = 1
     resolver.nameservers=[dns_server]
     try:
-        answer = resolver.query("www.gmail.com")
+        answer = resolver.query("www.github.com")
     except Exception:
         print("Dns ",dns_server," does not response")
         continue
-    print(dns_server,int(answer.response.time*1000),"ms")
-    if min_response > int(answer.response.time*1000):
-        min_response = int(answer.response.time*1000)
+
+    response_ms = int(answer.response.time * 1000)
+    print(dns_server, response_ms, "ms")
+    if min_response > response_ms:
+        min_response = response_ms
         min_response_server = dns_server
 
-print("Fast response: ",min_response_server," with ", min_response, " ms")
+    times.append(response_ms)
+    servers.append(dns_server)
+
+print("Fast response: ", min_response_server, " with ", min_response, " ms")
+
+times, servers = zip(*sorted(zip(times, servers)))
+plt.barh(servers, times)
+plt.show()
